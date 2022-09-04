cdf35f1fcfc0cc2be87038d0bfb0c14a55e3de81
Alvin Yanson <yansonalvin29@gmail.com>
2022-09-04 11:00:08 +0800

    route and query params

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,5 @@ Route::get('posts/{id}', function ($id) {
 })->where('id', '[0-9]+');
 
 Route::get('search', function(Request $request) {
-
+    return ($request->name . ' ' . $request->city);
 });
