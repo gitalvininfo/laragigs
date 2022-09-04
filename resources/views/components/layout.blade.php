cac0b406ea5fca094ba8634278e7262ee4002b27
Alvin Yanson <yansonalvin29@gmail.com>
2022-09-04 14:37:25 +0800

    done

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,7 +25,7 @@
 </head>
 
 <body>
-    <h1>Laragigs</h1>
+    {{-- <h1>Laragigs</h1> --}}
 
     <nav class="flex justify-between items-center mb-4">
         <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" /></a>
