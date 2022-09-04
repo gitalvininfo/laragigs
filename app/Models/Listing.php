c112ee4c713a508ae66f2f2f992308b9eb2e5c61
Alvin Yanson <yansonalvin29@gmail.com>
2022-09-04 12:33:25 +0800

    done in eloquenet

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,32 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti eos similique, dolorem aliquam exercitationem ea.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti eos similique, dolorem aliquam exercitationem ea.'
-            ],
-        ];
-    }
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
