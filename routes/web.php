536a864839fc876df60d45b2c21134af34512d8a
Alvin Yanson <yansonalvin29@gmail.com>
2022-09-04 13:55:32 +0800

    done

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 Route::get('/listings/{listing}', function (Listing $listing) {
     return view('listing', [
-        'listing' => Listing::find($listing)
+        'listing' => $listing
     ]);
 });
 
