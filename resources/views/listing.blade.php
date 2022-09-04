f8d5666f009ca9a1cc25d81ca24f115a827a79b5
Alvin Yanson <yansonalvin29@gmail.com>
2022-09-04 13:06:06 +0800

    done

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,7 +1,11 @@
-<h2>
-    {{ $listing['title'] }}
-</h2>
+@extends('layout')
 
-<p>
-    {{ $listing['description'] }}
-</p>
+@section('content')
+    <h2>
+        {{ $listing['title'] }}
+    </h2>
+
+    <p>
+        {{ $listing['description'] }}
+    </p>
+@endsection
